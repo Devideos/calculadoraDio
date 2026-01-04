710546adb6abb332d1e818b4e743fadb36b14d54
Devideos <davidpfdei@gmail.com>
2026-01-04 18:17:15 -0300

    acerto

---
+++ calculadora.js
@@ -0,0 +1,26 @@
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel = "";
+
+    if (vitorias <= 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return `O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`;
+}
+
+// Exemplos de execução
+console.log(calcularNivel(25, 10)); 
+console.log(calcularNivel(5, 7));   
